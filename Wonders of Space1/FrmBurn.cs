f6e0ca7a008e22e22548a85017053f01fc0b7d5e
Simonmax1 <51844738+Simonmax1@users.noreply.github.com>
2019-07-29 09:44:48 +1200

    I started coding the game and inserted the images for the game in the debug folder.

--- Wonders of Space1/FrmBurn.cs
+++ Wonders of Space1/FrmBurn.cs
@@ -12,9 +12,33 @@ namespace Wonders_of_Space1
 {
     public partial class FrmBurn : Form
     {
+        Graphics g; // declare the graphics objects
+        int x = 20, y = 20; // strating position of Meteors
+        int x1 = 35, y1 = 35;//strating position of Satellites
+        //Declare the rectangles to display the Aircraft,Meteors and Satellites in 
+        Rectangle area, area1, area2, area3, area4, area5, area6, area7, area8, area9, area10;
+        int x2 = 50, y2 = 290;//starting position of of the Aircraft
+        // Load our Three images from bin,debug folder
+        Image Aircraft = Image.FromFile(Application.StartupPath + @"\Aircraft.png");
+        Image Satellites = Image.FromFile(Application.StartupPath + @"\Satellites.png");
+        Image Meteors = Image.FromFile(Application.StartupPath + @"\Meteors.png");
+
+        
         public FrmBurn()
         {
             InitializeComponent();
+            area = new Rectangle(x2, y2, 30, 30);//Aircraft's rectangle
+            area1 = new Rectangle(x, y, 20, 20);//Meteor1's rectangle
+            area2 = new Rectangle(x, y, 21, 21);//Meteor2's rectangle
+            area3 = new Rectangle(x, y, 22, 22);//Meteor3's rectangle
+            area4 = new Rectangle(x, y, 23, 23);//Meteor4's rectnagle
+            area5 = new Rectangle(x, y, 24, 24);//Meteor5's rectangle
+            area6 = new Rectangle(x1, y1, 35, 35);//Satellite1's rectangle
+            area7 = new Rectangle(x1, y1, 36, 36);//Satellite2's rectangle
+            area8 = new Rectangle(x1, x1, 37, 37);//Satellite3's rectangle
+            area9 = new Rectangle(x1, x1, 38, 38);//Satellite4's rectangle
+            area10 = new Rectangle(x1, y1, 39, 39);//Satellite5's rectangle
+
         }
     }
 }
