0d73f80d1181ccce41125b82e0313c2e822c9c1d
Simonmax1 <51844738+Simonmax1@users.noreply.github.com>
2019-08-14 13:26:35 +1200

    I solved some errors got some help from josh. My meteors are moving side to side.

--- Wonders of Space1/FrmBurn.Designer.cs
+++ Wonders of Space1/FrmBurn.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.PnlGame = new System.Windows.Forms.Panel();
             this.Tmrmeteor = new System.Windows.Forms.Timer(this.components);
+            this.Tmrsat = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // PnlGame
@@ -38,7 +39,7 @@
             this.PnlGame.BackColor = System.Drawing.Color.Silver;
             this.PnlGame.Location = new System.Drawing.Point(12, 85);
             this.PnlGame.Name = "PnlGame";
-            this.PnlGame.Size = new System.Drawing.Size(407, 347);
+            this.PnlGame.Size = new System.Drawing.Size(686, 498);
             this.PnlGame.TabIndex = 0;
             this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
             // 
@@ -47,14 +48,18 @@
             this.Tmrmeteor.Enabled = true;
             this.Tmrmeteor.Tick += new System.EventHandler(this.Tmrmeteor_Tick);
             // 
+            // Tmrsat
+            // 
+            this.Tmrsat.Enabled = true;
+            // 
             // FrmBurn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 444);
+            this.ClientSize = new System.Drawing.Size(799, 636);
             this.Controls.Add(this.PnlGame);
             this.Name = "FrmBurn";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -64,6 +69,7 @@
 
         private System.Windows.Forms.Panel PnlGame;
         private System.Windows.Forms.Timer Tmrmeteor;
+        private System.Windows.Forms.Timer Tmrsat;
     }
 }
 
--- Wonders of Space1/FrmBurn.cs
+++ Wonders of Space1/FrmBurn.cs
@@ -17,75 +17,45 @@ namespace Wonders_of_Space1
         int x1 = 35, y1 = 35;//strating position of Satellites
         //Declare the rectangles to display the Aircraft,Meteors and Satellites in 
         Rectangle areaAircraft;
-        Rectangle[] area = new Rectangle[5];//area[1]to area[10] 
+        Rectangle[] areamet = new Rectangle[5];//area[1]to area[10] 
+        Rectangle[] areasat = new Rectangle[5];//area[1]to area[10]
         Random speed = new Random();
-        int[] meteorspeed = new int[11];
+        int[] meteorspeed = new int[5];
+        int[] satspeed = new int[5];
+        
         int x2 = 50, y2 = 290;//starting position of of the Aircraft
         // Load our Three images from bin,debug folder
         Image Aircraft = Image.FromFile(Application.StartupPath + @"\Aircraft.png");
         Image Satellites = Image.FromFile(Application.StartupPath + @"\Satellites.png");
         Image Meteors = Image.FromFile(Application.StartupPath + @"\Meteors.png");
 
+       
+
         public FrmBurn()
         {
-            InitializeComponent();
-            areaAircraft = new Rectangle(x2, y2, 30, 30);
-            meteorspeed[0] = speed.Next(5, 11);
-
-            for (int i = 0; i <= 11; i++)
-            {
-                area[0] = new Rectangle(x + 70 * 0, y, 40, 40);
-            }
-            for (int i = 0; i < 11; i++) 
-            {
-                area[1] = new Rectangle(x + 50 * 0, y, 20, 20);
-              
-            }
-            for (int i = 1; i < 11; i++)
-            {
-                area[2] = new Rectangle(x + 50 * 1, y, 20, 20);
-            }
-            for (int i = 2; i < 11; i++)
-            {
-                area[3] = new Rectangle(x = 50 * 2, y, 20, 20);
-            }
-            for (int i = 3; i < 11; i++)
-            {
-                area[4] = new Rectangle(x = 50 * 3, y, 20, 20);
-            }
-            for(int i = 4 ;i<11;i++)
-            {
-                area[5] = new Rectangle(x = 50*1,y,20,20);
-            }
            
-            for (int i = 5; i < 11; i++)
+            areaAircraft = new Rectangle(x2, y2, 30, 30);
+  
+            
+            for (int i = 0; i <= 4; i++)
             {
-                area[6] = new Rectangle(x = 50 * 5, y, 20, 20);
+                areamet[i] = new Rectangle(x2 + 70 * i, y2, 40, 40);
+                meteorspeed[i] = speed.Next(6, 15);
             }
-            for (int i = 0; i < 11; i++)
+            
+            for (int i = 0; i < 4; i++)
             {
-                area[7] = new Rectangle(x1 = 70 * 0, y1, 35, 35);
-            }
-            for (int i = 1; i < 11; i++)
-            {
-                area[8] = new Rectangle(x1 = 70 * 1, y1, 36, 36);
-            }
-            for (int i = 2; i < 11; i++)
-            {
-                area[9] = new Rectangle(x1 = 70 * 2, y1, 37, 37);
-            }
-            for (int i = 3; i < 11; i++)
-            {
-                area[10] = new Rectangle(x1 = 70 * 3, y1, 38, 38);
-            }
-            for (int i = 4; i < 11; i++)
-            {
-                area[11] = new Rectangle(x1 = 70 * 4, y1, 39, 39);
+                areasat[i] = new Rectangle(x1 + 70 * i, y1, 35, 35);
+                satspeed[i] = speed.Next(6, 15);
+
             }
 
 
 
-             
+
+
+
+            InitializeComponent();
         }
 
         private void PnlGame_Paint(object sender, PaintEventArgs e)
@@ -93,64 +63,43 @@ namespace Wonders_of_Space1
             // get the methods from the graphic's class to paint on the panel
             g = e.Graphics;
             // use the DrawImage method to draw the Aircraft on the panel
-            g.DrawImage(Aircraft, area[0]);
-            for (int i = 0; i < 11; i++) 
+            g.DrawImage(Aircraft, areaAircraft);
+            for (int i = 0; i <= 4; i++) 
             {
-                g.DrawImage(Meteors, area[1]);
+                g.DrawImage(Meteors, areamet[i]);
             }
-            for (int i = 1; i < 11; i++) 
+           
+            for(int i = 0; i <= 4; i++)
             {
-                g.DrawImage(Meteors, area[2]);
+                g.DrawImage(Satellites, areasat[i]);
             }
-            for (int i = 2; i < 11; i++)
-            {
-                g.DrawImage(Meteors, area[3]);
-            }
-            for (int i = 3; i < 11; i++)
-            {
-                g.DrawImage(Meteors, area[4]);
-            }
-            for(int i = 4; i < 11; i++)
-            {
-                g.DrawImage(Meteors, area[5]);
-            }
-            for (int i = 5; i < 11; i++)
-            {
-                g.DrawImage(Meteors, area[6]);
-            }
-            for(int i = 6; i < 11; i++)
-            {
-                g.DrawImage(Satellites, area[7]);
-            }
-            for(int i = 7; i < 11; i++)
-            {
-                g.DrawImage(Satellites, area[8]);
-            }
-            for(int i = 8; i < 11; i++)
-            {
-                g.DrawImage(Satellites, area[9]);
-            }
-            for (int i = 9; i < 11; i++)
-            {
-                g.DrawImage(Satellites, area[10]);
-            }
-            for(int i = 10; i < 11; i++)
-            {
-                g.DrawImage(Satellites, area[11]);
-            }
+          
            
             
         }
 
         private void Tmrmeteor_Tick(object sender, EventArgs e)
         {
-           for(int i =0;i<=10;i++)
+
+
+            for (int i=0; i<=4; i++)
             {
-                area[i].Y += meteorspeed[i];
-                if(area[i].Y>PnlGame.Height)
-                {
-                    area[i].Y = 20;
-                }
+                areamet[i].X += meteorspeed[i];
+            }
+
+            PnlGame.Invalidate();//meke the paint even fire to redraw the panel
+
+        }
+
+
+        private void Tmrsat_Tick(object sender, EventArgs e)
+        {
+
+
+            for (int i = 0; i <= 4; i++)
+            {
+
+                areasat[i].Y += satspeed[i];
             }
 
             PnlGame.Invalidate();//meke the paint even fire to redraw the panel
